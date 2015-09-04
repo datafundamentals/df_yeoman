127ce5268771ab9918c006c1cff08b8ad909ff63
petecarapetyan <pete@couldbe.net>
2015-09-04 13:37:39 -0500

    second try to commit real working code

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+# a silly step to keep doing but this makes sure to keep resetting
+execute "re run chown on affected directories" do 
+    command "chown -R vagrant:vagrant ~/.nvm && chown -R vagrant:vagrant /usr/local && chown -R vagrant:vagrant ~/npm-global"
+    action :run
+end
+
+execute "prepare polymer dependencies" do 
+    group "vagrant"
+    user "vagrant"
+    command "npm install -g yo bower grunt-cli gulp generator-polymer"
+    action :run
+end
