13cc8926a66853fb69732964c3138591b424165c
petecarapetyan <pete@couldbe.net>
2015-09-25 07:03:42 -0500

    catch up commit Sept 25 2015

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,15 +4,30 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-# a silly step to keep doing but this makes sure to keep resetting
-execute "re run chown on affected directories" do 
-    command "chown -R vagrant:vagrant ~/.nvm && chown -R vagrant:vagrant /usr/local && chown -R vagrant:vagrant ~/npm-global"
+execute "prepare polymer dependencies" do 
+    command "npm install -g yo bower grunt-cli gulp generator-polymer"
     action :run
 end
 
-execute "prepare polymer dependencies" do 
-    group "vagrant"
-    user "vagrant"
-    command "npm install -g yo bower grunt-cli gulp generator-polymer"
+# a silly step to keep doing but this makes sure to keep resetting
+execute "re run chown on affected directories" do 
+    command "chown -R vagrant:vagrant /usr/local"
     action :run
+end
+
+
+
+
+template '/home/vagrant/nodePreReqs.sh' do
+    owner "vagrant"
+    group "vagrant"
+    source 'nodePreReqs.sh.erb'
+    mode "0777"
+end
+
+template '/home/vagrant/.profile' do
+    owner "vagrant"
+    group "vagrant"
+    source 'dot_profile.erb'
+    mode "0777"
 end
